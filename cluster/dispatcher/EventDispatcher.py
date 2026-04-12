e32a82fbd788c166ea7e22ae0b476cd7946b2e1b
Marco Paggetti <marco.paggetti@studio.unibo.it>
2026-04-12 11:33:26 +0200

    Added base event dispatcher

---
+++ cluster/dispatcher/EventDispatcher.py
@@ -0,0 +1,15 @@
+class EventDispatcher:
+
+    def __init__(self): 
+        self.handlers = {}
+
+    def register_handler(self, event_type: EventType, fun: callable):
+        if event_type in self.handlers:
+            print("Event type already assigned!")
+        self.handlers[event_type] = fun
+
+    def dispatch(self, event: Event):
+        if event.type not in self.handlers:
+            raise Exception("Illegal argument exception!")
+        handler = self.handlers[event.type]
+        handler(event)
